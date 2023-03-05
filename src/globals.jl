0fae118ac931a6a7f0331e8235f7fd4d71705ca9
Lizzie <e.tilden@wustl.edu>
2023-03-05 17:12:34 -0600

    Completed Homework

--- src/globals.jl
+++ src/globals.jl
@@ -1,13 +1,14 @@
 export getchar, set_homedir, get_homedir
 
 charset = 'a':'z'
+charset::StepRange{Char, Int64}
+getchar(idx) = charset[idx]::Char
 
-getchar(idx) = charset[idx]
 
 homedir = "wrong"
 
 function set_homedir(path)
     global homedir = path
 end
-get_homedir() = homedir
+get_homedir() = homedir::String
 
